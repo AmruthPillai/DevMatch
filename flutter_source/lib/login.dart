bb0708d143902d84898c3f46a13766929aeb3099
DarshanGowda0 <darshan.gowda008@gmail.com>
2019-06-01 15:45:44 +0530

    login oauth

--- flutter_source/lib/login.dart
+++ flutter_source/lib/login.dart
@@ -1,6 +1,88 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class LoginScreen extends StatelessWidget {
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:uni_links/uni_links.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  StreamSubscription _subs;
+  String GITHUB_CLIENT_ID = 'Iv1.88f752f7e6b4c910';
+  String GITHUB_CLIENT_SECRET = '954b2ef4da1f0d87eb4714a01afbff97340e9bb1';
+
+  @override
+  void initState() {
+    _initDeepLinkListener();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _disposeDeepLinkListener();
+    super.dispose();
+  }
+
+  void _initDeepLinkListener() async {
+    _subs = getLinksStream().listen((String link) {
+      print(link);
+      _checkDeepLink(link);
+    }, cancelOnError: true);
+  }
+
+  void _checkDeepLink(String link) {
+    if (link != null) {
+      String code = link.substring(link.indexOf(RegExp('code=')) + 5);
+      loginWithGitHub(code).then((firebaseUser) {
+        print("LOGGED IN AS: " + firebaseUser.displayName);
+      }).catchError((e) {
+        print("LOGIN ERROR: " + e.toString());
+      });
+    }
+  }
+
+  Future<FirebaseUser> loginWithGitHub(String code) async {
+    //ACCESS TOKEN REQUEST
+    final response = await http.post(
+      "https://github.com/login/oauth/access_token",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: jsonEncode(GitHubLoginRequest(
+        clientId: GITHUB_CLIENT_ID,
+        clientSecret: GITHUB_CLIENT_SECRET,
+        code: code,
+      )),
+    );
+
+    GitHubLoginResponse loginResponse =
+        GitHubLoginResponse.fromJson(json.decode(response.body));
+
+    //FIREBASE STUFF
+    final AuthCredential credential = GithubAuthProvider.getCredential(
+      token: loginResponse.accessToken,
+    );
+
+    final FirebaseUser user =
+        await FirebaseAuth.instance.signInWithCredential(credential);
+
+    return user;
+  }
+
+  void _disposeDeepLinkListener() {
+    if (_subs != null) {
+      _subs.cancel();
+      _subs = null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,15 +107,31 @@ class LoginScreen extends StatelessWidget {
               child: Center(
                 child: Text(
                   'Find teammates for your next hackathon, smart and easy.',
-                  style: TextStyle(fontSize: 25, color: Colors.grey[800],),
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.grey[800],
+                  ),
                 ),
               ),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 20.0),
               child: MaterialButton(
-                onPressed: () {
+                onPressed: () async {
+                  String url = "https://github.com/login/oauth/authorize" +
+                      "?client_id=" +
+                      GITHUB_CLIENT_ID +
+                      "&scope=public_repo%20read:user%20user:email";
 
+                  if (await canLaunch(url)) {
+                    await launch(
+                      url,
+                      forceSafariVC: false,
+                      forceWebView: false,
+                    );
+                  } else {
+                    print("CANNOT LAUNCH THIS URL!");
+                  }
                 },
                 color: Colors.white,
                 child: Text(
@@ -47,4 +145,33 @@ class LoginScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+class GitHubLoginRequest {
+  String clientId;
+  String clientSecret;
+  String code;
+
+  GitHubLoginRequest({this.clientId, this.clientSecret, this.code});
+
+  dynamic toJson() => {
+        "client_id": clientId,
+        "client_secret": clientSecret,
+        "code": code,
+      };
+}
+
+class GitHubLoginResponse {
+  String accessToken;
+  String tokenType;
+  String scope;
+
+  GitHubLoginResponse({this.accessToken, this.tokenType, this.scope});
+
+  factory GitHubLoginResponse.fromJson(Map<String, dynamic> json) =>
+      GitHubLoginResponse(
+        accessToken: json["access_token"],
+        tokenType: json["token_type"],
+        scope: json["scope"],
+      );
 }
