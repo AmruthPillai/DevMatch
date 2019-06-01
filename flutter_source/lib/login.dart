bf6b88ca42961b24380fde3cafb037a9967c9a43
Amruth Pillai <im.amruth@gmail.com>
2019-06-01 15:39:28 +0530

    update login screen

--- flutter_source/lib/login.dart
+++ flutter_source/lib/login.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:DevMatch/button.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -8,41 +10,42 @@ class LoginScreen extends StatelessWidget {
         width: double.infinity,
         height: double.infinity,
         color: Colors.grey[50],
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Hero(
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 80.0, horizontal: 30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Hero(
                 tag: 'logo_tag',
                 child: Image.asset(
                   'assets/logo.png',
                 ),
               ),
-            ),
-            Expanded(
-              child: Center(
-                child: Text(
-                  'Find teammates for your next hackathon, smart and easy.',
-                  style: TextStyle(fontSize: 25, color: Colors.grey[800],),
+              Text(
+                'Find teammates for your next hackathon, smart and easy.',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey[800],
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-              child: MaterialButton(
-                onPressed: () {
-
-                },
-                color: Colors.white,
-                child: Text(
-                  'Github',
-                  style: TextStyle(color: Colors.redAccent),
+              Spacer(),
+              CustomButton(
+                backgroundColor: Colors.grey[900],
+                icon: Icon(Icons.lock),
+                buttonText: Text(
+                  'Login with GitHub',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[50],
+                  ),
                 ),
+                textColor: Colors.grey[900],
+                iconAlignment: Alignment.centerRight,
+                onPressed: () {},
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
